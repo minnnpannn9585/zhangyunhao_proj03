69e99436ec0f16bfe7f49a273b537bcbaeea0f65
xiaobai555555 <160624844+xiaobai555555@users.noreply.github.com>
2026-04-06 21:38:17 +0800

    111

--- Assets/Scripts/FoodData.cs
+++ Assets/Scripts/FoodData.cs
@@ -4,10 +4,10 @@
 public class FoodData : ScriptableObject
 {
     public string foodName;          // 食物名称
-    public Sprite foodSprite;        // 食物外观
-    
+    public Sprite foodSprite;        // 食物外观    
     public int stageOneScore;        // 阶段一得分
     public int stageTwoScore;        // 阶段二得分
     public int stageThreeScore;      // 阶段三得分
     public int stageFourScore;       // 阶段四得分
+    public float DropSpeed;            // 食物下落速度
 }
